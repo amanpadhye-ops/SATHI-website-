12816cd84b62b39660655d35dac693ca2d86f4cc
amanpadhye-ops <aman.padhye@gmail.com>
2026-05-25 21:23:36 +0530

    Add files via upload

---
+++ shared.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  document.body.classList.toggle('mobile-nav-open');
+}
+document.addEventListener('click', function(e) {
+  if (!e.target.closest('nav')) {
+    document.body.classList.remove('mobile-nav-open');
+  }
+});
